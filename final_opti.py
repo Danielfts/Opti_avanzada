a159a88084f1caefbe28f3a8575ba3610cd12df1
ggarzon09 <119578703+ggarzon09@users.noreply.github.com>
2022-12-01 04:28:23 -0500

    Update final_opti.py

--- final_opti.py
+++ final_opti.py
@@ -7,18 +7,31 @@ import pyflakes
 from openpyxl import*
 
 
-
-
-
-
 book = load_workbook("/Users/spheal/Desktop/a.xlsx")
 sheet = book.active 
 datos = book.active
 
+demanda = [] #demanda
+for fila in range(1,24):
+        demanda.append(sheet.cell(  12,fila+5).value)
+
+miuk= [] #mius
+for fila in range(1,24):
+        miuk.append(sheet.cell(  14,fila+5).value)
+columnas= []
+matriz=[]
+for i in range(1,9):
+    fila= []
+
+    for j in range(1,25):
+        fila.append(sheet.cell(i+18,j+5).value)
+    matriz.append(fila)
+        
+matriz =np.array(matriz)
+dfm=pd.DataFrame(matriz)
 
 
+demanda = np.array(demanda)
 
-demanda = {} #demanda
-for fila in range(6,12):
-        indice = sheet.cell(fila,14).value
-        vector[upz]= sheet.cell(fila,7).value
+
+    
